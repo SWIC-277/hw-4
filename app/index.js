a3888e4c1a52f25e74b78bee8a898341369ad065
Manav Misra <manavm@visionwebsoft.com>
2022-09-28 20:18:59 -0500

    Pass calcAvg test

--- app/index.js
+++ app/index.js
@@ -1,6 +1,5 @@
-export const calcAvg = () => {
-  throw new Error("Not implemented");
-};
+export const calcAvg = (nums) =>
+  nums.reduce((total, num) => total + num, 0) / nums.length;
 
 export const calcCartTotal = () => {
   throw new Error("Not implemented");
