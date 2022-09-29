5dc8cb83662eeb08256cff8f063dfb2f562484a4
Manav Misra <manavm@visionwebsoft.com>
2022-09-28 20:50:30 -0500

    Pass test for tallyBycategory

--- app/index.js
+++ app/index.js
@@ -1,6 +1,16 @@
 export const calcAvg = (nums) =>
   nums.reduce((total, num) => total + num, 0) / nums.length;
 
-export const calcCartTotal = () => {
-  throw new Error("Not implemented");
-};
+export const calcCartTotal = (cart) =>
+  cart.reduce((total, item) => total + item.price * item.qty, 0);
+
+export const tallyByCategory = (items) =>
+  items.reduce((tally, item) => {
+    if (tally[item.category]) {
+      tally[item.category] += item.qty;
+    } else {
+      tally[item.category] = item.qty;
+    }
+
+    return tally;
+  }, {});
